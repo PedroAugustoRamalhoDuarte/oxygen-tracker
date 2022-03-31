b3ca366e49472d1620bc73b78385cd2cb773b37a
Waliff Cordeiro <waliffcordeiro@gmail.com>
2022-03-31 11:53:21 -0300

    Draw face and forehead rects and save temp pictures
    Co-authored-by: PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
    Co-authored-by: Joao Pedro Assis <joaopedroassisdossantos@gmail.com>

--- app/src/main/java/com/example/oxygenmeasurement/OpencvCamera.java
+++ app/src/main/java/com/example/oxygenmeasurement/OpencvCamera.java
@@ -1,7 +1,6 @@
 package com.example.oxygenmeasurement;
 
 import static org.opencv.core.Core.absdiff;
-import static org.opencv.core.Core.flip;
 import static org.opencv.core.Core.split;
 
 import androidx.annotation.NonNull;
@@ -9,11 +8,9 @@ import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.app.Activity;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Environment;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.SurfaceView;
 import android.view.View;
@@ -23,17 +20,14 @@ import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
-import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
-import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -180,15 +174,31 @@ public class OpencvCamera extends Activity implements CameraBridgeViewBase.CvCam
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
 
         mRGBA = inputFrame.rgba();
-        Mat mRGBAT = mRGBA.t();
-        Core.flip(mRGBA.t(), mRGBAT, 1);
-        Imgproc.resize(mRGBAT, mRGBAT, mRGBA.size());
+        Mat mRGBAT = mRGBA;
+//        Mat mRGBAT = mRGBA.t();
+//        Core.flip(mRGBA.t(), mRGBAT, 1);
+//        Imgproc.resize(mRGBAT, mRGBAT, mRGBA.size());
 
         // ROI
+
+        int h2 = mRGBAT.width();
+        int w2 = mRGBAT.height();
+        int h_rect2 = h2/2+100;
+        int w_rect2 = w2 * 3 / 5;
+        int xStart2 = (w2 - w_rect2 + 150) / 2;
+        int yStart2 = (h2 - h_rect2 - 100) / 3;
+        int xEnd2 = (w2 + w_rect2 + 150) / 2;
+        int yEnd2 = (h2 + h_rect2) / 3;
+
+        // Draw rectangle
+        Imgproc.rectangle(mRGBAT, new Point
+                (xStart2, yStart2), new Point(
+                xEnd2, yEnd2), new Scalar(255, 255, 0), 5);
+
         int h = mRGBAT.width();
         int w = mRGBAT.height();
-        int h_rect = h / 4;
-        int w_rect = w * 3 / 4;
+        int h_rect = h / 10;
+        int w_rect = w * 3 / 12;
         int xStart = (w - w_rect + 150) / 2;
         int yStart = (h - h_rect - 200) / 3;
         int xEnd = (w + w_rect + 150) / 2;
@@ -213,6 +223,17 @@ public class OpencvCamera extends Activity implements CameraBridgeViewBase.CvCam
 
                 videoFramesBlue.add(channels.get(0).clone());
                 videoFramesRed.add(channels.get(2).clone());
+
+                File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+                String filename = "temp.jpg";
+                File file = new File(path, filename);
+                boolean bool;
+                filename = file.toString();
+                bool = Imgcodecs.imwrite(filename, mROI);
+                if (bool)
+                    Log.i(TAG, "SUCCESS writing image to external storage");
+                else
+                    Log.i(TAG, "Fail writing image to external storage");
 
                 Log.i(TAG, String.valueOf(videoFramesRed));
             }
